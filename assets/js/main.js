103fcbb34d9754e46533817768617dd11a66faa5
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-21 15:04:11 +0100

    Upload file

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*
+    1. Chiedi all’utente il suo nome
+    2. Chiedi il suo cognome
+    3. Chiedi il suo colore preferito
+    4. Genera la password costituita dalla stringa nomecognomecolorepreferito21
+*/
+
