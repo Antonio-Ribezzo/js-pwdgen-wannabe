df5b9e755b3faf58cc3d1e6ef1b20433f9f519ad
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-21 15:05:28 +0100

    JS script

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,14 @@
     4. Genera la password costituita dalla stringa nomecognomecolorepreferito21
 */
 
+const name = prompt('Qual è il tuo nome?');
+
+const surname = prompt('Qual è il tuo cognome?');
+
+const color = prompt('Qual è il tuo colore preferito?');
+
+let password = name + surname + color;
+
+console.log(password);
+
+document.getElementById('pass').innerHTML = `<h2>${password}21</h2>`
